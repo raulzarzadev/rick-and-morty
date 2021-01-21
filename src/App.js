c1407aae398eef0cf08c636422baa7cbfaaa4ce5
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-20 18:09:29 -0600

    Card and Grid start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FindInput from "./components/FindInput";
+import "./styles/header.css";
+import "./styles/main.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div>logo</div>
+        <div>Tu personaje favorito de Rick and Morty</div>
+        <nav>
+          <ul>
+            <li>inicio</li>
+            <li>acerca de</li>
+          </ul>
+        </nav>
       </header>
+      <main className="main">
+        <FindInput />
+      </main>
+      <footer>
+        Una app creada por <a href="https://raulzarza.com">RZ</a>
+      </footer>
     </div>
   );
 }
---
+++ src/components/FindInput.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import "../styles/charter-card.css";
+import "../styles/characters.css";
+
+function FindInput() {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  async function getCharacterByName(name) {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${name}`
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  const [charters, setCharters] = useState([]);
+
+  useEffect(() => {
+    getCharacterByName(search)
+      .then((res) => setCharters(res.results))
+      .catch((err) => console.log(err));
+  }, [search]);
+
+  console.log(charters);
+
+  return (
+    <div>
+      <div>
+        <input onChange={handleChange} type="text" name="find" id="find" />
+      </div>
+      <div>Seleccina uno</div>
+      <div className="characters">
+        {charters.map((charter) => (
+          <div className="characters_item">
+            <CharterCard charter={charter} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CharterCard({ charter }) {
+  console.log(charter);
+  return (
+    <div className="charter">
+      <div className="charter_image">
+        <img
+          className="charter_image--img"
+          src={charter.image}
+          alt={`charter-${charter.name}`}
+        />
+      </div>
+      <div className="charter_info">
+        <div className="charter_info--item">
+          <div className="item-left">Nombre:</div>
+          <div className="item-rigth"> {charter.name}</div>
+        </div>
+        <div className="charter_info--item">
+          <div className="item-left">Origen:</div>
+          <div className="item-rigth"> {charter.origin.name}</div>
+        </div>
+        <div className="charter_info--item">
+          <div className="item-left">Especie:</div>
+          <div className="item-rigth"> {charter.species}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FindInput;
